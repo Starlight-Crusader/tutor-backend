7508d34a0e737ec14fdd8735eb53a8cbcaf4d590
AKalamaghin <arteom.kalamaghin@isa.utm.md>
2022-10-12 08:47:07 +0300

    Fixed venv

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ak/Documents/python_practice/env"
+set -gx VIRTUAL_ENV "/home/ak/Documents/python_practice/tutor-backend/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
