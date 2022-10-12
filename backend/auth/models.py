b427f01d372074fb2b90af8f6653ed04474aac81
AKalamaghin <arteom.kalamaghin@isa.utm.md>
2022-10-12 16:00:31 +0300

    Backup for test migration

--- backend/auth/models.py
+++ backend/auth/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# class Sertificate(models.Model): ...
+
+class User(models.Model):
+    emal = models.CharField(max_length=32)
+    password = models.CharField(max_length=32)
+
+    first_name = models.CharField(max_length=16)
+    last_name = models.CharField(max_length=16)
+
+    is_student = models.BooleanField()
+    is_pupil = models.BooleanField()
+    is_admin = model.BooleanField()
+
+    date_of_birth = model.DateField()
+    location = model.CharField(max_length=32)
+
+    profile_picture = model.CharField(max_length=64)
+    
