c98966f6f1e5dc29b682e6e590a404221fb8e0b5
g4mrpr0n <georgeana.globa@gmail.com>
2022-10-24 11:10:09 +0300

    (development) Changed the model for Certificates: Removed the subject_name field

--- backend/certificates/models.py
+++ backend/certificates/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-#from subjects import models
-#try making subjects a foreign key
+
 
 class Certificate(models.Model):
-    subject_name = models.CharField(max_length=100)
     document_path = models.FileField(upload_to="docs/certificates")
     is_verified = models.BooleanField(default=False)
